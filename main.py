ec88b48fdbc5e8b310287d20e77693582bbc3b9e
centerfield77 <tatsunori.dia@gmail.com>
2023-02-19 00:33:41 +0900

    ✨ main

---
+++ main.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import os,time
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+def main():
+    # URL
+    TOP_PAGE_URL = 'https://id.jobcan.jp/users/sign_in'
+
+    # ログイン情報
+    MAIL_ADDRESS = 'MAIL_ADDRESS'
+    PASSWORD = 'PASSWORD'
+
+    # CSSセレクタ
+    MAIL_INPUT_SELECTOR = '#user_email'
+    PASSWORD_INPUT_SELECTOR = '#user_password'
+    LOGIN_BUTTON_SELECTOR = '#login_button'
+    KINTAI_PAGE_LINK_TAB_SELECTOR = '#jbc-app-links > ul > li:nth-child(3) > a'
+    DAKOKU_BUTTON_SELECTOR = '#adit-button-push'
+
+
+    # seleniumの初期化
+    path = os.getcwd()
+    driverPath = path + '/crawler/chromedriver'
+    headlessPath = path + '/crawler/headless-chromium'
+    options = Options()
+    options.binary_location = headlessPath
+    driver = webdriver.Chrome(driverPath, options=options)
+
+    # ページを開く
+    driver.get(TOP_PAGE_URL)
+    time.sleep(2)
+
+    # ログイン
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, MAIL_INPUT_SELECTOR)))
+    mail_input = driver.find_element_by_css_selector(MAIL_INPUT_SELECTOR)
+    password_input = driver.find_element_by_css_selector(PASSWORD_INPUT_SELECTOR)
+    login_button = driver.find_element_by_css_selector(LOGIN_BUTTON_SELECTOR)
+    mail_input.send_keys(MAIL_ADDRESS)
+    password_input.send_keys(PASSWORD)
+    login_button.click()
+    time.sleep(2)
+
+    # 勤怠ページへ
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, KINTAI_PAGE_LINK_TAB_SELECTOR)))
+    kintai_page_link_tab = driver.find_element_by_css_selector(KINTAI_PAGE_LINK_TAB_SELECTOR)
+    kintai_page_link_tab.click()
+    time.sleep(2)
+
+    # 打刻
+    driver.switch_to.window(driver.window_handles[1])
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, DAKOKU_BUTTON_SELECTOR)))
+    dakoku_button = driver.find_element_by_css_selector(DAKOKU_BUTTON_SELECTOR)
+    print(dakoku_button.text)
+    dakoku_button.click()
+
+    driver.quit()
