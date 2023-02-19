425415c73687e4e0336cc61b0873edb9b476cf5c
centerfield77 <tatsunori.dia@gmail.com>
2023-02-19 22:43:24 +0900

    🔨

---
+++ prepareChromeDriver.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+mkdir -p src/bin
+curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-37/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
+unzip headless-chromium.zip -d src/bin/
+rm headless-chromium.zip
+
+curl -SL https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip > chromedriver.zip
+unzip chromedriver.zip -d src/bin/
+rm chromedriver.zip
